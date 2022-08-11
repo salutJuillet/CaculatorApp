55aa0ca174e97eb56057b3ce95c5e5fd96b69495
salutJuillet <salut.juilllet@gmail.com>
2022-08-11 17:35:36 +0900

    필요없는 import 삭제

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Caculator from './components/Caculator';
 
 export default function App() {
